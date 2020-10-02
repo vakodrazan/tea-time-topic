55f8949945fe6c1003edbbffac43ef726734bbc3
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-02 09:43:58 +0300

    Store the fetch data as a default export in another file

---
+++ fetchData.js
@@ -0,0 +1,10 @@
+const endpoint = `https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c70331ca4a16bb99/raw/6da767327041de13693181c2cb09459b0a3657a1/topics.json`;
+// Default export
+async function fetching() {
+    const res = await fetch(endpoint);
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
+
+export default fetching;
