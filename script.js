02dbc52bdb44edf004b8268279b8c34882722bf6
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-02 09:42:59 +0300

    Call the showlist function

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const endpoint = `https://gist.githubusercontent.com/Pinois/93afbc4a061352a0c70331ca4a16bb99/raw/6da767327041de13693181c2cb09459b0a3657a1/topics.json`;
+// Import from file module
+import { showList } from './list.js'
 
+let topic = [];
 
-async function fetching() {
-    const res = await fetch(endpoint);
-    const data = await res.json();
-    console.log(data);
-    return data;
-}
+showList(topic);
 
-fetching();
