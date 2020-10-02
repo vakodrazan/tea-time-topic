aa5de339e4ba6526ae7cce71d0bc2d7f4f1b2bec
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-02 15:09:03 +0300

    Listen to the score event

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Import from file module
 import { showList } from './list.js';
-import { form } from './elements.js';
+import { form, list } from './elements.js';
 import { addNewTopic } from './elements.js';
-
+import { scoreVoted } from './votedScore.js';
+import { init } from './localStorage.js';
 // let topic = [];
-
 
 showList();
 form.addEventListener('submit', addNewTopic);
+list.addEventListener('click', scoreVoted);
+init();
